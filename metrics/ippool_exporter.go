9b77ed9002d54b5d0d8e3a00c4e42ea19823ccb5
zeyuliu <zeyuliu@harmonycloud.cn>
2020-08-24 13:43:27 +0800

    fix: update metrics describe

--- metrics/ippool_exporter.go
+++ metrics/ippool_exporter.go
@@ -26,8 +26,8 @@ func (m *IPPoolExporter) Export(p *protocol.IPPool, ch chan<- prometheus.Metric)
 	for _, ipp := range m.IPPResultList {
 		labels := m.labelStrategy.LabelNames()
 
-		ippTotal := prometheus.NewDesc("calico_ippool"+"_total", "total of ippol", labels, nil)
-		ippInuse := prometheus.NewDesc("calico_ippool"+"_inuse", "inuse of ippol", labels, nil)
+		ippTotal := prometheus.NewDesc("calico_ippool"+"_total", "total of ippool", labels, nil)
+		ippInuse := prometheus.NewDesc("calico_ippool"+"_inuse", "inuse of ippool", labels, nil)
 		l := m.labelStrategy.LabelValues(&ipp)
 		ch <- prometheus.MustNewConstMetric(ippTotal, prometheus.GaugeValue, ipp.Total, l...)
 		ch <- prometheus.MustNewConstMetric(ippInuse, prometheus.GaugeValue, ipp.Inuse, l...)
